ef36d16c7f28279302c893d25da054a78621e6a4
Francesco Chicchiriccò <ilgrosso@apache.org>
2016-03-11 13:46:26 +0100

    Generating the consolidated list of dependencies for LICENSE and NOTICE

--- src/main/java/net/tirasa/ilgrosso/lngenerator/Main.java
+++ src/main/java/net/tirasa/ilgrosso/lngenerator/Main.java
@@ -15,9 +15,89 @@
  */
 package net.tirasa.ilgrosso.lngenerator;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.maven.index.artifact.Gav;
+import org.apache.maven.index.artifact.M2GavCalculator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public final class Main {
 
-    public static void main(final String[] args) {
+    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
 
+    private static final String LOCAL_M2_REPO = System.getProperty("user.home").
+            concat(File.separator).concat(".m2").concat(File.separator).concat("repository");
+
+    private static final Path LOCAL_M2_REPO_PATH = Paths.get(LOCAL_M2_REPO);
+
+    private static final M2GavCalculator GAV_CALCULATOR = new M2GavCalculator();
+
+    private static final String[] CONSOLIDATING_GROUP_IDS = new String[] {
+        "net.tirasa.connid", "org.slf4j", "org.springframework.security", "org.springframework", "io.swagger", 
+        "org.activiti", "com.googlecode.wicket-jquery-ui"
+    };
+
+    private static final String[] CONSOLIDATING_GROUP_PREFIXES = new String[] {
+        "com.fasterxml.jackson" };
+
+    public static void main(final String[] args) throws IOException {
+        LOG.debug("Local Maven repo is {}", LOCAL_M2_REPO);
+        LOG.debug("Path argument is {}", args[0]);
+
+        Set<String> keys = new HashSet<>();
+
+        Files.walk(Paths.get(args[0])).
+                filter(Files::isRegularFile).
+                map((final Path path) -> path.getFileName().toString()).
+                filter((String path) -> path.endsWith(".jar")).
+                sorted().
+                forEach((filename) -> {
+                    try (Stream<Path> stream = Files.find(LOCAL_M2_REPO_PATH, 10,
+                            (path, attr) -> String.valueOf(path.getFileName().toString()).equals(filename))) {
+
+                        String fullPath = stream.sorted().map(String::valueOf).collect(Collectors.joining("; "));
+                        if (fullPath.isEmpty()) {
+                            LOG.warn("Could not find {} in the local Maven repo", filename);
+                        } else {
+                            String path = fullPath.substring(LOCAL_M2_REPO.length() + 1);
+                            Gav gav = GAV_CALCULATOR.pathToGav(path);
+                            if (gav == null) {
+                                LOG.error("Invalid Maven path: {}", path);
+                            } else if (!gav.getGroupId().startsWith("org.apache.")
+                                    && !gav.getGroupId().startsWith("commons-")
+                                    && !gav.getGroupId().equals("xml-apis")) {
+
+                                if (ArrayUtils.contains(CONSOLIDATING_GROUP_IDS, gav.getGroupId())) {
+                                    keys.add(gav.getGroupId());
+                                } else {
+                                    boolean prefixFound = false;
+                                    for (String prefix : CONSOLIDATING_GROUP_PREFIXES) {
+                                        if (gav.getGroupId().startsWith(prefix)) {
+                                            prefixFound = true;
+                                            keys.add(prefix);
+                                        }
+                                    }
+
+                                    if (!prefixFound) {
+                                        keys.add(gav.getGroupId() + ":" + gav.getArtifactId());
+                                    }
+                                }
+                            }
+                        }
+                    } catch (IOException e) {
+                        LOG.error("While looking for Maven artifacts from the local Maven repo", e);
+                    }
+                });
+
+        keys.stream().sorted().forEach(System.out::println);
     }
 }
